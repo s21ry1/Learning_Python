171a8dd8e1402f74a36b6d955a47f621186c7759
s21ry1 <surya7official@gmail.com>
2024-08-15 02:48:34 +0530

    applying input function

--- string.py
+++
@@ -1,6 +0,0 @@
-print("hello"+ " "+ "world"+ " " + "hello"+ " " + "world")
-# hello world hello world
-
-print("hello"+ " "+ "world"+ " " +input("what is your name"))
-
-print("hey"+ " "+ input("what is your name")+" "+ "how are you")
